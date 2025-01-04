2776523d0121e12b1687058e48edee31d02740a0
callumleach <callumleach31@gmail.com>
2025-01-04 18:04:24 +0000

    add quicksync support to jellyfin

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,21 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      vaapiVdpau
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
+
   # Set your time zone.
   time.timeZone = "Europe/London";
 
